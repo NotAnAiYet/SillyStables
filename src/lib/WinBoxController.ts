268fdc4ca55eada887767efcdd7b9e3e8122657c
Highom <highomlp@gmail.com>
2026-09-02 12:55:00 +0200

    etc: added icons to windows

--- src/lib/WinBoxController.ts
+++ src/lib/WinBoxController.ts
@@ -1,15 +1,22 @@
 import WinBox from 'winbox/src/js/winbox.js';
+import globe from '../assets/media/icons/globe.svg';
+import clock from '../assets/media/icons/clock.svg';
 
 type Win = {
-  content: HTMLElement;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  title: string;
+    content: HTMLElement;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    title: string;
 };
 
 const backgroundColor = '#0056e4';
+
+const iconMap: Record<string, string> = {
+    "about": globe.src,
+    "last-updated": clock.src,
+};
 
 export class WinBoxController {
     private windows = new Map<string, Win>();
@@ -43,16 +50,7 @@ export function initWinBoxStartup(windows: Map<string, Win> = new Map()) {
 
     windows.forEach((win, id) => {
         if (defaultOpen.has(id)) {
-            new WinBox({
-                id: id+'-winbox',
-                title: win.title,
-                background: backgroundColor,
-                x: win.x,
-                y: win.y,
-                width: win.width + 'px',
-                height: win.height + 'px',
-                mount: win.content.cloneNode(true),
-            });
+            initWindow(id, win);
         }
         // Add event listeners to open button for this window
         const openBtn = document.querySelector<HTMLElement>(`[data-open-window="${id}"]`);
@@ -61,16 +59,25 @@ export function initWinBoxStartup(windows: Map<string, Win> = new Map()) {
                 return;
             }
             
-            new WinBox({
-                id: id+'-winbox',
-                title: win.title,
-                background: backgroundColor,
-                x: win.x,
-                y: win.y,
-                width: win.width + 'px',
-                height: win.height + 'px',
-                mount: win.content.cloneNode(true),
-            });
+            initWindow(id, win);
         });
     });
 };
+
+function initWindow(id: string, win: Win) {
+    var winbox = new WinBox({
+        id: id + '-winbox',
+        title: win.title,
+        background: backgroundColor,
+        x: win.x,
+        y: win.y,
+        width: win.width + 'px',
+        height: win.height + 'px',
+        mount: win.content.cloneNode(true),
+    });
+
+    if (iconMap[id]) {
+        console.log(`Setting icon for window "${id}" to "${iconMap[id]}"`);
+        winbox.setIcon(iconMap[id]);
+    }
+}
